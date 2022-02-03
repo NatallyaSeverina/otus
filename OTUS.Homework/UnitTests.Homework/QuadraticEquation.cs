c9b55171caf5ebe4674a4dd3fd92470cb02d5c99
NatallyaSeverina <natasha.awesomeyo@gmail.com>
2022-02-03 15:20:21 +0300

    Implemented fuction QuadraticEquation.Solve, added unit-tests

---
+++ OTUS.Homework/UnitTests.Homework/QuadraticEquation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Homework
+{
+	public static class QuadraticEquation
+	{
+		public static double[] Solve(double a, double b, double c)
+		{
+			if (Math.Abs(a) < 0.000000001
+				|| a is Double.NaN
+				|| a is Double.NegativeInfinity
+				|| a is Double.PositiveInfinity
+				|| b is Double.NaN
+				|| b is Double.NegativeInfinity
+				|| b is Double.PositiveInfinity
+				|| c is Double.NaN
+				|| c is Double.NegativeInfinity
+				|| c is Double.PositiveInfinity)
+			{
+				throw new ArgumentException();
+			}
+			var d = b*b - 4*a*c;
+			if (d < 0)
+			{
+				return new double[0];
+			}
+			else if (Math.Abs(d) < 0.000000001)
+			{
+				return new double[] { -b /(2 * a), -b / (2 * a) };
+			}
+			else if (d > 0 )
+			{
+				return new double[] { (-b + Math.Sqrt(d))/(2* a), (-b - Math.Sqrt(d)) / (2 * a) };
+			}
+			return new double[0];
+		}
+	}
+}
--- OTUS.Homework/UnitTests.Homework/QuadraticEquationTests.cs
+++ OTUS.Homework/UnitTests.Homework/QuadraticEquationTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace UnitTests.Homework
@@ -10,9 +12,78 @@ namespace UnitTests.Homework
 		}
 
 		[Test]
-		public void GithubCISetUpTest()
+		public void Solve_NegativeDiscriminant_ReturnsEmptyArrayWithoutRoots()
 		{
-			Assert.Pass();
+			// Arrange x^2+1 = 0 
+			var a = 1;
+			var b = 0;
+			var c = 1;
+			
+			// Act
+			var result = QuadraticEquation.Solve(a, b, c);
+
+			// Assert
+			Assert.That(result, Is.Empty);
+		}
+
+		[Test]
+		public void Solve_PositiveDiscriminant_ReturnsArrayWithTwoDistinctRoots()
+		{
+			// Arrange D around zero
+			var a = 1;
+			var b = 0.001;
+			var c = 0;
+			var expectedRoots = new double[2] {0, -0.001};
+			
+			// Act
+
+			var result = QuadraticEquation.Solve(a, b, c);
+			// Assert
+			Assert.IsTrue(result != null && result.Length == 2);
+			Assert.AreEqual(expectedRoots, result);
+		}
+
+		[Test]
+		public void Solve_ZeroDiscriminant_ReturnsArrayWithTwoTheSameRoots()
+		{
+			// Arrange x^2+2x+1 
+			var a = 1;
+			var b = 2;
+			var c = 1;
+			var expectedRoots = new double[2] {-1, -1};
+			
+			// Act
+
+			var result = QuadraticEquation.Solve(a, b, c);
+			// Assert
+			Assert.IsTrue(result != null && result.Length == 2 && result[0] == result[1]);
+			Assert.AreEqual(expectedRoots, result);
+		}
+
+		[Test]
+		public void Solve_ZeroAParameter_ThrowException()
+		{
+			// Arrange  a = 0
+			var a = 0;
+			var b = 2;
+			var c = 1;
+					
+			// Act
+			// Assert
+			Assert.Throws<ArgumentException>(() => QuadraticEquation.Solve(a, b, c));		
+		}
+
+		[Test]
+		public void Solve_DoubleNaNAsAParameter_ThrowException()
+		{
+			// Arrange  a = 0
+			var a = Double.NaN;
+			var b = 2;
+			var c = 1;
+					
+			// Act
+			// Assert
+			Assert.Throws<ArgumentException>(() => QuadraticEquation.Solve(a, b, c));		
 		}
 	}
 }
